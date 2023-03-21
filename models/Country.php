f1f2c013bf49da859a01c010b4d29d8bce214157
Caca404 <christia10k@hotmail.com>
2023-03-21 16:06:47 -0300

    feat: using Gii to generate code

--- models/Country.php
+++ models/Country.php
@@ -2,11 +2,47 @@
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
+use Yii;
 
-class Country extends ActiveRecord
+/**
+ * This is the model class for table "countries".
+ *
+ * @property string $sigla
+ * @property string $nome
+ * @property int $populacao
+ */
+class Country extends \yii\db\ActiveRecord
 {
-    public static function tableName(){
-        return "countries";
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'countries';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['sigla', 'nome'], 'required'],
+            [['sigla', 'nome'], 'string'],
+            [['populacao'], 'integer'],
+            [['sigla'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'sigla' => 'Sigla',
+            'nome' => 'Nome',
+            'populacao' => 'Populacao',
+        ];
     }
 }
---
+++ views/country/view.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/** @var yii\web\View $this */
+/** @var app\models\Country $model */
+
+$this->title = $model->sigla;
+$this->params['breadcrumbs'][] = ['label' => 'Countries', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="country-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'sigla' => $model->sigla], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'sigla' => $model->sigla], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'sigla:ntext',
+            'nome:ntext',
+            'populacao',
+        ],
+    ]) ?>
+
+</div>
